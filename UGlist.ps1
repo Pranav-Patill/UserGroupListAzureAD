6fdb6a4267b937b101579657c2157aa52cf10d50
Pranav-Patill <98013998+Pranav-Patill@users.noreply.github.com>
2022-12-23 20:32:14 +0530

    Delete UGlist.ps1

--- UGlist.ps1
+++
@@ -1,8 +0,0 @@
-Connect-AzureAD 
-
-$PathCsv = "D:\UserList.csv"
-$ServicePrincipalList = Get-AzureADServicePrincipal -ObjectId fdde9722-4526-49b7-be9f-9c621f642815
-   
-foreach($servicePrincipal in $ServicePrincipalList){
-    Get-AzureADServiceAppRoleAssignment -ObjectId $servicePrincipal.ObjectID | Select-Object  PrincipalDisplayName, PrincipalType | Export-Csv -Path $PathCsv -NoTypeInformation -Append
-}
