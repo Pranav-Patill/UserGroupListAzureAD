0108caa4b34b7dfab520e77020dc57b4b857021c
Pranav-Patill <98013998+Pranav-Patill@users.noreply.github.com>
2022-12-26 13:41:50 +0530

    Update UGlist.ps1

--- UGlist.ps1
+++ UGlist.ps1
@@ -1,12 +1,13 @@
 Connect-AzureAD 
 
 $PathCsv = "D:\UserList.csv"
-$ServicePrincipalList = Get-AzureADServicePrincipal -ObjectId fb7f9e29-e757-4dd4-b2f0-8a22e12c37ec
+$ServicePrincipalList = Get-AzureADServicePrincipal -ObjectId fb7f9e29-e757-4dd4-b2f0-8a22e12c37ec #Give the Object ID of the application you want to get data
    
 foreach($servicePrincipal in $ServicePrincipalList){
     Get-AzureADServiceAppRoleAssignment -ObjectId $servicePrincipal.ObjectID |Select-Object ResourceDisplayName, PrincipalDisplayName, PrincipalType, Id | Export-Csv -Path $PathCsv -NoTypeInformation -Append
 }
 
+#Give the Applcation ID of the application in "--id" parameter
 az ad app show --id 18ffccc4-c4f8-4092-905c-a3825bdc7955 --query "appRoles[*].{AppRoleName:displayName,AppID:id}" --output tsv > "D:\L.csv"
 
 
